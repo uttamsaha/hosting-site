f63a045065953488ad081c56f5e5be97cdfca510
uttamsaha <uttamsaha982@gmail.com>
2022-07-12 22:40:24 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -10,3 +10,14 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    //if pageY offset > 100px
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
