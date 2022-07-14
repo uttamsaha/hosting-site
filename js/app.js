e41f4ac6e383ae42c9259b480ca33be79c15d8a2
uttamsaha <uttamsaha982@gmail.com>
2022-07-14 12:55:41 +0600

    full responsive

--- js/app.js
+++ js/app.js
@@ -9,6 +9,17 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    }
+  }
 });
 
 
